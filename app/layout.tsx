8a2a915fbdd1de9a06bbc3fb2f544768fed22bd9
Raheem Qudus <84246065+Quddos@users.noreply.github.com>
2025-04-12 20:54:55 +0530

    bugBAE "[20:47:11.758] Running build in Washington, D.C., USA (East) – iad1 [20:47:11.946] Running "vercel build" [20:47:12.335] Vercel CLI 41.5.0 [20:47:12.652] Running "install" command: `npm install`... [20:47:17.514] npm warn deprecated @esbuild-kit/esm-loader@2.6.5: Merged into tsx: https://tsx.is [20:47:17.563] npm warn deprecated @esbuild-kit/core-utils@3.3.2: Merged into tsx: https://tsx.is [20:47:28.670] [20:47:28.671] added 431 packages, and audited 432 packages in 16s [20:47:28.671] [20:47:28.671] 147 packages are looking for funding [20:47:28.672]   run `npm fund` for details [20:47:28.688] [20:47:28.689] 4 moderate severity vulnerabilities [20:47:28.689] [20:47:28.689] To address all issues (including breaking changes), run: [20:47:28.689]   npm audit fix --force [20:47:28.690] [20:47:28.690] Run `npm audit` for details. [20:47:28.748] Detected Next.js version: 15.3.0 [20:47:28.749] Running "npm run build" [20:47:28.867] [20:47:28.868] > admission-portal@0.1.0 build [20:47:28.868] > next build [20:47:28.868] [20:47:29.634] Attention: Next.js now collects completely anonymous telemetry regarding usage. [20:47:29.634] This information is used to shape Next.js' roadmap and prioritize features. [20:47:29.635] You can learn more, including how to opt-out if you'd not like to participate in this anonymous program, by visiting the following URL: [20:47:29.636] https://nextjs.org/telemetry [20:47:29.636] [20:47:29.698]    ▲ Next.js 15.3.0 [20:47:29.698] [20:47:29.782]    Creating an optimized production build ... [20:47:44.157] Failed to compile. [20:47:44.158] [20:47:44.158] ./app/(auth)/sign-in/[[...sign-in]]/page.tsx [20:47:44.158] Module not found: Package path ./app-beta is not exported from package /vercel/path0/node_modules/@clerk/nextjs (see exports field in /vercel/path0/node_modules/@clerk/nextjs/package.json) [20:47:44.158] [20:47:44.158] https://nextjs.org/docs/messages/module-not-found [20:47:44.158] [20:47:44.158] ./app/(auth)/sign-up/[[...sign-up]]/page.tsx [20:47:44.158] Module not found: Package path ./app-beta is not exported from package /vercel/path0/node_modules/@clerk/nextjs (see exports field in /vercel/path0/node_modules/@clerk/nextjs/package.json) [20:47:44.158] [20:47:44.159] https://nextjs.org/docs/messages/module-not-found [20:47:44.159] [20:47:44.159] ./app/layout.tsx [20:47:44.159] Module not found: Package path ./app is not exported from package /vercel/path0/node_modules/@clerk/nextjs (see exports field in /vercel/path0/node_modules/@clerk/nextjs/package.json) [20:47:44.159] [20:47:44.159] https://nextjs.org/docs/messages/module-not-found [20:47:44.159] [20:47:44.174] [20:47:44.174] > Build failed because of webpack errors [20:47:44.206] Error: Command "npm run build" exited with 1 [20:47:44.916] "

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ClerkProvider } from '@clerk/nextjs/app';
+import { ClerkProvider } from '@clerk/nextjs';
 import { Toaster } from 'sonner';
 
 const geistSans = Geist({
